e1e276b58d1a44e5fe9845161805a9c17a78569d
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-11-04 12:48:14 +0100

    api

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
-            intro: 'Welcome to my Vue template',
+            actors:[]
+            // actors: {
+            //     name: '',
+            //     id: '',
+            //     birthday: '',
+            // },
+            // newName: null,
+            // newId: null,
+            // newBirthday: null
         }
+
+
     },
+    mounted() {
+        this.myMethod()
+    },
+
     methods: { 
         myMethod() {
-            
+            axios.get('http://localhost:5154/Api/Actors').then(response => {this.actors = response.data})
         },
+
+        
     },
     computed: {
         myComputed() { 
@@ -15,3 +31,5 @@ const app = Vue.createApp({
         }
     }
 });
+
+app.mount("#app")
